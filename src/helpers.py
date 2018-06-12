Bug fix on cutoff calculation

src/helpers.py
@@ -57,22 +57,45 @@ def mult3dsorted(im_sorted, filt_sorted, b):
     mult_out = np.multiply(im_sorted, filt_sorted)
     
     cumsum = np.cumsum(mult_out) + b
-
-    cutoff_point = np.argmax(np.diff( (cumsum<=0).astype(int) ))
+    max_ind = np.argmax(cumsum)
+    max_val = cumsum[max_ind]
+    
+    cutoff_point = np.argmax((cumsum[max_ind:]<=0))
     
     if cutoff_point == 0:
         ws_cnt = mult_out.shape[0]
         midsum = max(cumsum[-1], 0)
     else:
-        ws_cnt = cutoff_point
+        ws_cnt = cutoff_point + max_ind
         midsum = 0
     
-    max_ind = np.argmax(cumsum)
-    max_val = cumsum[max_ind]
+    
+    
+
     
     return midsum, ws_cnt, max_ind, max_val
 
-def conv3dsorted(im, filt, b):
+def conv3d(im, filt, b):
+    lx = im.shape[0]
+    ly = im.shape[1]
+    lz = filt.shape[3]
+    
+    pad_size = int((int(filt.shape[0])-1)/2)
+    im_padded = np.pad(im, ((pad_size, pad_size), (pad_size, pad_size), (0, 0)), mode='constant', constant_values=(0, 0))
+
+    ofmap = np.zeros((lx,ly,lz))
+    
+    for f in range(lz):
+        for i in range(lx):
+            for j in range(ly):
+                ofmap[i,j,f] = np.sum(np.multiply(im_padded[i:i+filt.shape[0], j:j+filt.shape[1], :], filt[:,:,:,f])) + b[f]
+    
+    ofmap[ofmap<0] = 0
+    
+    return ofmap
+                
+                
+def conv3dsorted(im, filt, b, perc_procastinate):
     start_time = time.time()
     
     lx = im.shape[0]
@@ -84,7 +107,7 @@ def conv3dsorted(im, filt, b):
     
     ofmap = np.zeros((lx,ly,lz))
     
-    filt_ind = sortConvFilters(filt, 0)
+    filt_ind = sortConvFilters(filt, perc_procastinate)
     
     ws_cnt = 0
     mac_cnt = filt.shape[0]*filt.shape[1]*filt.shape[2]*im.shape[0]*im.shape[1]*filt.shape[3]
@@ -156,15 +179,15 @@ def sortConvFilters(filt, perc_procrastinate):
         negW_sorted = sorted(negW)
 
         s = int(len(posW_sorted) * (1-perc_procrastinate))
-        for i in posW_sorted[0:s+1]:              
+        for i in posW_sorted[0:s]:              
             filt_ind[f].append(i[1])
 
         for i in negW_sorted:              
             filt_ind[f].append(i[1])
     
-        for i in posW_sorted[s+1:]:              
+        for i in posW_sorted[s:]:              
             filt_ind[f].append(i[1])
-            
+                
     return filt_ind
 
 def max_pool(im):
@@ -190,6 +213,10 @@ def max_pool(im):
     print("--- %s seconds ---" % (time.time() - start_time))
     return out
     
+def fc(im,w,b):
+    out = np.matmul(im,  w) + b
+    out[out<0] = 0
+    return out
     
 def fcOutput(im,w,b):
     out = np.matmul(im,  w) + b
@@ -227,14 +254,15 @@ def vectormultSorted(im_sorted, w_sorted, b):
     mult = np.multiply(im_sorted, w_sorted)
     
     cumsum = np.cumsum(mult) + b
+    max_ind = np.argmax(cumsum)
     
-    cutoff_point = np.argmax(np.diff( (cumsum<=0).astype(int) ))
+    cutoff_point = np.argmax((cumsum[max_ind:]<=0))
     
     if cutoff_point == 0:
         ws_cnt = mult.shape[0]
         midsum = max(cumsum[-1], 0)
     else:
-        ws_cnt = cutoff_point
+        ws_cnt = cutoff_point + max_ind
         midsum = 0
 
     return midsum, ws_cnt

src/main.py
@@ -8,6 +8,8 @@ import numpy as np
 import helpers
 import time
 
+import sys
+
 from keras.applications.vgg16 import VGG16
 from keras.applications.vgg16 import preprocess_input
 from keras.preprocessing import image
@@ -15,12 +17,14 @@ from keras import backend as K
 
 model = VGG16(weights='imagenet')
 
-im_dir = '/home/acy/Downloads/ILSVRC2012_img_val/'
+im_dir = sys.argv[1:2][0]
+no_imgs = int(sys.argv[2:3][0])
+
+img_ids, im_list = helpers.choose_subset(im_dir, no_imgs)
 
 logfile = open("log.txt","w") 
 
-no_subset = 10
-img_ids, im_list = helpers.choose_subset(im_dir, no_subset)
+perc_procastinate = 0
 
 org_top1_cnt = 0
 org_top5_cnt = 0
@@ -35,7 +39,7 @@ for i, im_name in enumerate(im_list):
     x = preprocess_input(x)
     
     pred = model.predict(x)
-        
+    
     start_time = time.time()
     
     total_mac_cnt = 0
@@ -45,25 +49,28 @@ for i, im_name in enumerate(im_list):
         logfile.write("Layer: " + layer.name+ "\n")
         logfile.flush()
         
-        f = K.function([model.input], [layer.input])
-        layer_input = f([x])[0]
-    
-        f = K.function([model.input], [layer.output])
-        layer_output = f([x])[0]
-                
-        if layer.name.__eq__("block1_conv1"):
-            next_layer_input = layer_output
+        #f = K.function([model.input], [layer.input])
+        #layer_input = f([x])[0]
+
+        #f = K.function([model.input], [layer.output])
+        #layer_output = f([x])[0]
         
+        if layer.name.__eq__("block1_conv1"):
+            #next_layer_input = layer_output
+            [weights, biases] = layer.get_weights()
+            ofmap = helpers.conv3d(x[0,:,:,:], weights, biases)
+            next_layer_input = np.expand_dims(ofmap, axis=0)
+            
         elif layer.name.__contains__("conv"):
             [weights, biases] = layer.get_weights()
             
-            ofmap, mac_cnt, ws_cnt, mp_cnt = helpers.conv3dsorted(next_layer_input[0,:,:,:], weights, biases)
+            ofmap, mac_cnt, ws_cnt, mp_cnt = helpers.conv3dsorted(next_layer_input[0,:,:,:], weights, biases, perc_procastinate)
             total_mac_cnt = total_mac_cnt + mac_cnt
             total_ws_cnt = total_ws_cnt + ws_cnt
             
-            print("Max error: ", np.max(np.abs(layer_output-ofmap)))
+            #print("Max error: ", np.max(np.abs(layer_output-ofmap)))
             print("# of mac: ", mac_cnt, " # of ws: ", ws_cnt, " , % of skipped: ", 1 - float(ws_cnt)/mac_cnt )
-            logfile.write("Max error: " + str(np.max(np.abs(layer_output-ofmap)))+ "\n")
+            #logfile.write("Max error: " + str(np.max(np.abs(layer_output-ofmap)))+ "\n")
             logfile.write("# of mac: " + str(mac_cnt) + " # of ws: " + str( ws_cnt) + " , % of skipped: " + str(1 - float(ws_cnt)/mac_cnt) + "\n")
             
             if model.layers[idx+1].name.__contains__("pool"):
@@ -86,9 +93,9 @@ for i, im_name in enumerate(im_list):
             total_mac_cnt = total_mac_cnt + mac_cnt
             total_ws_cnt = total_ws_cnt + ws_cnt
             
-            print("Max error: ", np.max(np.abs(layer_output-ofmap)))
+            #print("Max error: ", np.max(np.abs(layer_output-ofmap)))
             print("# of mac: ", mac_cnt, " # of ws: ", ws_cnt, " , % of skipped: ", 1 - float(ws_cnt)/mac_cnt )
-            logfile.write("Max error: " + str(np.max(np.abs(layer_output-ofmap))) + "\n")
+            #logfile.write("Max error: " + str(np.max(np.abs(layer_output-ofmap))) + "\n")
             logfile.write("# of mac: " + str(mac_cnt) + " # of ws: " + str( ws_cnt) + " , % of skipped: " + str(1 - float(ws_cnt)/mac_cnt) + "\n")            
             next_layer_input = np.expand_dims(ofmap, axis=0)
             
@@ -115,7 +122,7 @@ for i, im_name in enumerate(im_list):
     
     logfile.write("Top1: " + str(top1correct) + " Top5: " + str(top5correct)+ "\n")
     
-print('Original top1 accuracy: ', float(org_top1_cnt) / no_subset)
-print('Original top5 accuracy: ', float(org_top5_cnt) / no_subset)
-print('WS top1 accuracy: ', float(ws_top1_cnt) / no_subset)
-print('WS top5 accuracy: ', float(ws_top5_cnt) / no_subset)
+print('Original top1 accuracy: ', float(org_top1_cnt) / no_imgs)
+print('Original top5 accuracy: ', float(org_top5_cnt) / no_imgs)
+print('WS top1 accuracy: ', float(ws_top1_cnt) / no_imgs)
+print('WS top5 accuracy: ', float(ws_top5_cnt) / no_imgs)